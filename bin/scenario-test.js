73b5aaa8a213ed29cfee36586741cc9275072b9a
seungmin paik <sm.paik@ridi.com>
2018-12-06 15:23:47 +0900

    fail check added

--- bin/scenario-test.js
+++ bin/scenario-test.js
@@ -49,17 +49,16 @@ const scenarioTest = (collection, environment) => {
         // reporters: 'cli',// error가 있으면 그것만 출력하고 전체 결과는 reporter는 사용하지 않는다.
     };
     newman.run(options, checkErrorAndSummary);
-}
+};
 
 const main = async () => {
     try {
         const [collections, environment] = await Promise.all([getCollections(), getEnvironment()]);
         collections.forEach(collection => scenarioTest(collection, environment))
     } catch (err) {
-        noticeError(err);
-        throw new Error(`test fail`);
+        console.log(err);
+        process.exit(1);
     }
-
 };
 
 main();
