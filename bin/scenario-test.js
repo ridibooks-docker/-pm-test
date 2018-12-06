b7bc28f5da75c2402b90f2d636adcfb9320b46e4
seungmin paik <sm.paik@ridi.com>
2018-12-06 14:45:47 +0900

    check all error

--- bin/scenario-test.js
+++ bin/scenario-test.js
@@ -1,8 +1,8 @@
 const newman = require('newman');
-const { getCollections, getEnvironment } = require('./requests');
+const {getCollections, getEnvironment} = require('./requests');
 
 const noticeError = (error) => {
-  console.log(error); // 우선은 로그만 찍어본다.
+    console.log(error); // 우선은 로그만 찍어본다.
 };
 
 const getParsedRequest = (request) => ({
@@ -14,46 +14,52 @@ const getParsedRequest = (request) => ({
 });
 
 const checkErrorAndSummary = (err, summary) => {
-  const collectionName = summary.collection.name;
-  let errorRaised = false;
-  if (err !== null) {
-      noticeError(err);
-      throw new Error(` collections: "${collectionName}" test fail`); 
-  }
-  for (const exceution of summary.run.executions) {
-      if (exceution.requestError) {
-          noticeError({collectionName,error: exceution.requestError, request: getParsedRequest(exceution.request)});
-          errorRaised = true;
-          continue;
-      }
-      for (const assertion of exceution.assertions) {
-          if (assertion.error) {
-              errorRaised = true;
-              noticeError({collectionName, error: assertion.error, request: getParsedRequest(exceution.request)});
-          }
-      }
-  }
+    const collectionName = summary.collection.name;
+    let errorRaised = false;
+    if (err !== null) {
+        noticeError(err);
+        throw new Error(` collections: "${collectionName}" test fail`);
+    }
+    for (const exceution of summary.run.executions) {
+        if (exceution.requestError) {
+            noticeError({collectionName, error: exceution.requestError, request: getParsedRequest(exceution.request)});
+            errorRaised = true;
+            continue;
+        }
+        for (const assertion of exceution.assertions) {
+            if (assertion.error) {
+                errorRaised = true;
+                noticeError({collectionName, error: assertion.error, request: getParsedRequest(exceution.request)});
+            }
+        }
+    }
 
-  if (errorRaised) {
-      throw new Error(` collections: "${collectionName}" test fail`);
-  }
-  console.log(`collection: "${collectionName}" passed`);
+    if (errorRaised) {
+        throw new Error(` collections: "${collectionName}" test fail`);
+    }
+    console.log(`collection: "${collectionName}" passed`);
 };
 
-const scenarioTest = (collection, environment)=>{
-  const options = {
-    collection,
-    environment,
-    insecure: true,
-    ignoreRedirects: true,
-    // reporters: 'cli',// error가 있으면 그것만 출력하고 전체 결과는 reporter는 사용하지 않는다.
-  };
-  newman.run(options, checkErrorAndSummary);
+const scenarioTest = (collection, environment) => {
+    const options = {
+        collection,
+        environment,
+        insecure: true,
+        ignoreRedirects: true,
+        // reporters: 'cli',// error가 있으면 그것만 출력하고 전체 결과는 reporter는 사용하지 않는다.
+    };
+    newman.run(options, checkErrorAndSummary);
 }
 
 const main = async () => {
-  const [collections, environment] = await Promise.all([getCollections(), getEnvironment()]);
-  collections.forEach(collection=>scenarioTest(collection,environment))
+    try {
+        const [collections, environment] = await Promise.all([getCollections(), getEnvironment()]);
+        collections.forEach(collection => scenarioTest(collection, environment))
+    } catch (err) {
+        noticeError(err);
+        throw new Error(`test fail`);
+    }
+
 };
 
 main();
